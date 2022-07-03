b7a6916373cd8502697a671337976792868e301e
Wilson <30907754+acioliwilson@users.noreply.github.com>
2022-07-03 19:17:25 -0300

    test

---
+++ js/about.8e779609.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunkresultadosdamega"]=self["webpackChunkresultadosdamega"]||[]).push([[443],{381:function(a,e,n){n.r(e),n.d(e,{default:function(){return l}});var s=n(252);const u={class:"about"},t=(0,s._)("h1",null,"This is an about page",-1),r=[t];function c(a,e){return(0,s.wg)(),(0,s.iD)("div",u,r)}var o=n(744);const d={},i=(0,o.Z)(d,[["render",c]]);var l=i}}]);
+//# sourceMappingURL=about.8e779609.js.map
